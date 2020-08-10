676d6b5f91acd750158fb29ab0e0531365106008
Travis Deig <travisdeig@gmail.com>
2020-08-10 14:17:00 -0400

    Using Redux

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,12 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { createForms } from 'react-redux-form';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { Campsites } from './campsites';
 import { Comments } from './comments';
 import { Partners } from './partners';
 import { Promotions } from './promotions';
-import { InitialFeedback } from './forms';
 
 
 export const ConfigureStore = () => {
@@ -15,10 +13,7 @@ export const ConfigureStore = () => {
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions,
-            ...createForms({
-                feedbackForm: InitialFeedback
-            })
+            promotions: Promotions
         }),
         applyMiddleware(thunk, logger)
     );
